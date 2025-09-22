29aa73661a910124dacd382f66b03249ac870844
Ricardo Pimienta <pimientaglez@gmail.com>
2025-09-21 21:18:33 -0600

    Enhance server configuration for production and development environments; update axios base URL handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,15 @@ import notesRoutes from "./routes/notesRoutes";
 import { connectDB } from "./config/db";
 import rateLimiter from "./middleware/rateLimiter";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
 const MONGO_DB_URI =
   process.env.MONGO_DB_URI || "mongodb://localhost:27017/thinkboard";
 
@@ -19,12 +22,22 @@ if (!MONGO_DB_URI) {
 }
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" })); // Enable CORS for frontend
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" })); // Enable CORS for frontend
+}
+
 app.use(express.json()); // for parsing application/json
 app.use(rateLimiter); // Apply rate limiting middleware
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "/dist", "/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// in production, the backend is served from the same origin
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
